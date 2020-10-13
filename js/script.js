3e6ac32d684bb5979541e2bc0d6dfe5f10a194c9
dhvani28 <69980396+dhvani28@users.noreply.github.com>
2020-10-13 13:38:02 +0530

    new design deployed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,53 @@
-$(function () {
-  $(window).on('scroll', function () {
-      if ( $(window).scrollTop() > 10 ) {
-          $('.navbar').addClass('active');
-      } else {
-          $('.navbar').removeClass('active');
-      }
+$(function() {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.container-fluid').addClass('active');
+        } else {
+            $('.container-fluid').removeClass('active');
+        }
+    });
   });
+ 
+const selectElement = function (element) {
+     return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click',function(){
+    body.classList.toggle('open');
 });
+
+//scroll revel
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+ 
